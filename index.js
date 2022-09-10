e397016f2c90d1a50680824f4f2de3c7d1a2061c
sims <simulatedsilver@protonmail.com>
2022-09-09 21:34:49 -0700

    additional assets and further ui reconfig :shit:

--- index.js
+++ index.js
@@ -24,14 +24,23 @@ function redrawAvatar(){
   let context = document.getElementById('avatarCanvas').getContext('2d')
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   //sort avatar so that the images get stacked correctly...
-  let sortedAvatar = [null,null,null,null,null,null]
+  let sortedAvatar = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
   avatar.forEach((element) => {
     if(element.className == 'background') sortedAvatar[0]=element
     else if(element.className =='auras') sortedAvatar[1]=element
     else if(element.className == 'race') sortedAvatar[2]=element
-    else if(element.className == 'eyes') sortedAvatar[3]=element
-    else if(element.className == 'hairstyles') sortedAvatar[4]=element
+    else if(element.className == 'shirt') sortedAvatar[3]=element
+    else if(element.className == 'necklace') sortedAvatar[4]=element
     else if(element.className == 'mouths') sortedAvatar[5]=element
+    else if(element.className =='lens') sortedAvatar[6]=element
+    else if(element.className == 'eyes') sortedAvatar[7]=element
+    else if(element.className == 'eyebrows') sortedAvatar[8]=element
+    else if(element.className == 'face') sortedAvatar[9]=element
+    else if(element.className == 'earring') sortedAvatar[10]=element
+    else if(element.className =='glasses') sortedAvatar[11]=element
+    else if(element.className == 'hairstyles') sortedAvatar[12]=element
+    else if(element.className == 'hat') sortedAvatar[13]=element
+    else if(element.className == 'extras') sortedAvatar[14]=element
   });
   sortedAvatar.forEach((element) => {
     if(element != null)
